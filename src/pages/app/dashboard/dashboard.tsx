94ce88a7b5263086ca4daa35bd24c0489cdc06e4
joão pedro cruz <joaocruzzdev@gmail.com>
2025-02-17 14:01:28 -0300

    cards da pagina dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,7 +1,19 @@
+import { MothRevenueCard } from "./components/moth-revenue-card";
+import { DayOrdersAmountCard } from "./components/day-orders-amount-card";
+import { MonthOrdersAmountCard } from "./components/moth-orders-amount-card";
+import { MonthCanceledOrdersAmountCard } from "./components/month-canceled-orders-amount-card";
+
 export function DashBoard() {
    return (
       <div className="flex flex-col gap-4">
          <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+         <div className="grid grid-cols-4 gap-4">
+            <MothRevenueCard />
+            <MonthOrdersAmountCard />
+            <DayOrdersAmountCard />
+            <MonthCanceledOrdersAmountCard />
+         </div>
       </div>
    )
 }
